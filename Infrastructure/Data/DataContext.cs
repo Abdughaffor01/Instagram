8052472caa96a6d7fd9554fba667603232a84775
shahronnn <shahrompayvand@gmail.com>
2023-11-09 15:01:07 +0500

    dgfghj

--- Infrastructure/Data/DataContext.cs
+++ Infrastructure/Data/DataContext.cs
@@ -1,12 +1,11 @@
 namespace Infrastructure.Data;
 
-public class DataContext : IdentityDbContext<IdentityUser>
+public class DataContext : IdentityDbContext<User>
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
         
     }
-    public DbSet<User> Users { get; set; }
     public DbSet<Profile> Profiles { get; set; }
     public DbSet<Post> Posts  { get; set; }
     public DbSet<PostViewUser> PostViewUsers { get; set; }
