d1107c5fa76190419450c778e91860743d40c72a
Abdughaffor01 <ghaffor987849660@gmail.com>
2023-11-11 12:41:31 +0500

    test

--- Domain/Entities/ApplicationUser.cs
+++ Domain/Entities/ApplicationUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Entities;
 
-public class User : IdentityUser
+public class ApplicationUser : IdentityUser
 {
     public Profile Profile { get; set; }
 
